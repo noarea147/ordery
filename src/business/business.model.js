54bb0d078cfd22fd1c0f6b33bf3d1f2ec04c9a75
noarea147 <95benmbarek@gmail.com>
2023-08-07 00:57:59 +0100

    lets goo!! i can see the success

--- src/business/business.model.js
+++ src/business/business.model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const businessSchema = new Schema(
   {
-    BusinessName: String,
+    businessName: String,
     description: String,
     location: {
       lng: Number,
